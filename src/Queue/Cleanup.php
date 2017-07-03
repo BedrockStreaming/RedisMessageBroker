0298b7d046e1f4abd8cf74ba732d2ed82d52bafe
Olivier Mansour <olivier.mansour@m6.fr>
2017-07-03 17:50:49 +0200

    cleanup methods

--- src/Queue/Cleanup.php
+++ src/Queue/Cleanup.php
@@ -6,37 +6,47 @@ use M6Web\Component\RedisMessageBroker\Message;
 
 /**
  * Class Cleanup
- * clean the working list for a queue
+ * clean the working list or the ready message list for a queue
+ * those methods are slow and should be not used
  */
 class Cleanup extends AbstractQueueTool
 {
     /**
      * this method clean old message in the working list
      *
-     * @param int $maxAge maximum age of a message
+     * @param int  $maxAge             maximum age of a message
+     * @param bool $eraseReadyMessages try to erase ready messages too ?
+     *
+     * @return int number of erased messages
      */
-    public function cleanOldMessages(int $maxAge)
+    public function cleanOldMessages(int $maxAge, bool $eraseReadyMessages = false): int
     {
-        // TODO
+        $r = 0;
+        $hasToDelete = function (Message $message) use ($maxAge) {
+            return (time() - $message->getCreatedAt()->format('U')) > $maxAge; // if message was created before $maxAge
+        };
+        $r += $this->cleanMessage($this->redisClient->keys($this->queue->getWorkingListPrefixName().'*'), $hasToDelete);
+
+        if ($eraseReadyMessages) {
+            $r += $this->cleanMessage($this->queue->getListNames(), $hasToDelete);
+        }
+
+        return $r;
     }
 
-    private function cleanMessage(callable $hasTodelete)
+    private function cleanMessage(string $lists, callable $hasToDelete): int
     {
-        // TODO
-        foreach ($this->getWorkingLists() as $redisList) {
-            $listLen = $this->redisClient->llen($redisList);
+        $r = 0;
+        foreach ($lists as $list) {
+            $listLen = $this->redisClient->llen($list);
             for ($i = 1; $i < $listLen; ++$i) {
-                /** @var Message $message */
-                $message = $this->redisClient->rpoplpush($redisList, $redisList);
+                $message = $this->redisClient->rpoplpush($list, $list);
+                if ($hasToDelete(Message::unserializeMessage($message))) {
+                    $r += $this->redisClient->lrem($list, 0, $message);
+                }
             }
         }
-    }
 
-    /**
-     * delete the redis lists
-     */
-    public function cleanReadyMessages()
-    {
-        // TODO
+        return $r;
     }
 }
